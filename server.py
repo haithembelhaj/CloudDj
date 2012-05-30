2d137780556af1f09a65df01992012893b2fbdd8
Haithem <haythem.belhaj@gmail.com>
2012-05-30 02:28:58 +0200

    ports

--- server.py
+++ server.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template, request
-import requests
+import requests, os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+	return render_template('index.html')
 
 @app.route('/stream')
 def stream():
@@ -14,4 +14,5 @@ def stream():
 
 
 if __name__ == '__main__':
-    app.run(port= 28974)
+	port = int(os.environ.get('PORT', 5000))
+	app.run(host='0.0.0.0', port=port)
